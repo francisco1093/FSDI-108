18d039afc6f72fff8949995cf1704c6959c83993
Jesus <fcardenas1093@gmail.com>
2021-06-01 20:52:29 -0700

    initial commit session 3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NavBar from "./components/navBar";
+import Footer from "./components/footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import Catalog from "./components/catalog"; 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <h1 className="App-title">Sport Fran</h1>
+      <NavBar></NavBar>
+      <Catalog></Catalog>
+      <Footer></Footer>
     </div>
-  );
+  ); 
+
+  
 }
 
 export default App;
+
+//funtion based comp.
